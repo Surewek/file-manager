444196663091d2bdf66346354d7494220e1da7dd
Vitaly Vegera <surewek@gmail.com>
2024-10-12 12:58:11 +0300

    feat: add add command logic

--- src/files-commands/add.js
+++ src/files-commands/add.js
@@ -1,8 +1,30 @@
+import path from 'path';
+import isExists from '../utils/exist-checker.js';
+import fs from 'fs/promises';
+import messages from '../data/messages.js';
+import errors from '../data/error-messages.js';
+
 async function add(currentPath, commandArguments) {
   try {
+    const argumentsCount = commandArguments.length;
 
+    if (argumentsCount !== 1) {
+      throw new Error(errors.invalidInputMessage);
+    }
+
+    const [fileName] = commandArguments;
+    const targetPath = path.resolve(currentPath, fileName);
+    const isTargetPathExists = await isExists(targetPath);
+
+    if (isTargetPathExists) {
+      throw new Error(errors.invalidInputMessage);
+    }
+
+    await fs.writeFile(targetPath, '');
+
+    process.stdout.write(messages.getCurrentDirectoryMessage(currentPath));
   } catch (error) {
-      process.stderr.write(error.message);
+    process.stderr.write(error.message);
   }
 };
 
