247236f116dc6de5e4dcd2c21435eaac67c549d4
Vitaly Vegera <surewek@gmail.com>
2024-10-12 13:37:28 +0300

    feat: add cp command logic

--- src/files-commands/cp.js
+++ src/files-commands/cp.js
@@ -1,9 +1,45 @@
-async function cp(currentPath, commandArguments) {
-    try {
+import path from 'path';
+import { createWriteStream, createReadStream } from 'fs';
+import isExists from '../utils/exist-checker.js';
+import errors from '../data/error-messages.js';
 
-    } catch (error) {
-        process.stderr.write(error.message);
+async function cp(currentPath, commandArguments) {
+  try {
+    const argumentsCount = commandArguments.length;
+
+    if (argumentsCount !== 2) {
+      throw new Error(errors.invalidInputMessage);
     }
+
+    const [source, dest] = commandArguments;
+
+    const sourcePath = path.resolve(currentPath, source);
+    const destPath = path.resolve(currentPath, dest);
+
+    const isCurrentPathExists = await isExists(sourcePath);
+    const isTargetPathExists = await isExists(destPath);
+
+    if (!isCurrentPathExists) {
+      throw new Error(errors.invalidInputMessage);
+    }
+
+    if (isTargetPathExists) {
+      throw new Error(errors.invalidInputMessage);
+    }
+
+    const readStream = createReadStream(sourcePath);
+    const writeStream = createWriteStream(destPath);
+
+    await new Promise((resolve, reject) => {
+      readStream
+        .pipe(writeStream)
+        .on('finish', resolve)
+        .on('error', reject)
+    });
+
+  } catch (error) {
+    process.stderr.write(error.message);
+  }
 };
 
 export default cp;
