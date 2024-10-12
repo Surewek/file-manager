a91a5e04f0bc78c343ae58938a00bd4f24ca69f2
Vitaly Vegera <surewek@gmail.com>
2024-10-12 13:15:12 +0300

    feat: add rn command logic

--- src/files-commands/rn.js
+++ src/files-commands/rn.js
@@ -1,8 +1,35 @@
+import path from 'path';
+import isExists from '../utils/exist-checker.js';
+import fs from 'fs/promises'
+import errors from '../data/error-messages.js';
+
 async function rn(currentPath, commandArguments) {
   try {
+    const argumentsCount = commandArguments.length;
+
+    if (argumentsCount !== 2) {
+      throw new Error(errors.invalidInputMessage);
+    }
+
+    const [oldName, newName] = commandArguments;
+    const oldNamePath = path.resolve(currentPath, oldName);
+    const newNamePath = path.resolve(currentPath, newName);
+
+    const isCurrentPathExists = await isExists(oldNamePath);
+    const isTargetPathExists = await isExists(newNamePath);
+
+    if (!isCurrentPathExists) {
+      throw new Error(errors.invalidInputMessage);
+    }
+
+    if (isTargetPathExists) {
+      throw new Error(errors.invalidInputMessage);
+    }
+
+    await fs.rename(oldNamePath, newNamePath);
 
   } catch (error) {
-      process.stderr.write(error.message);
+    process.stderr.write(error.message);
   }
 };
 
