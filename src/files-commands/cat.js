c52c2c05a9ee27e8c5fc2178c7ebf7dd64b98cb8
Vitaly Vegera <surewek@gmail.com>
2024-10-12 12:45:51 +0300

    feat: add cat command logic

--- src/files-commands/cat.js
+++ src/files-commands/cat.js
@@ -1,8 +1,40 @@
+import path from 'path';
+import isExists from '../utils/exist-checker.js';
+import { createReadStream } from 'fs';
+import readline from 'readline';
+import errors from '../data/error-messages.js';
+import messages from '../data/messages.js';
+
 async function cat(currentPath, commandArguments) {
   try {
+    const argumentsCount = commandArguments.length;
+
+    if (argumentsCount !== 1) {
+      throw new Error(errors.invalidInputMessage);
+    }
+
+    const [fileName] = commandArguments;
+    const targetPath = path.resolve(currentPath, fileName);
+
+    const isTargetPathExists = await isExists(targetPath);
+
+    if (!isTargetPathExists) {
+      throw new Error(errors.invalidInputMessage);
+    }
+
+    const rl = readline.createInterface({
+      input: createReadStream(targetPath),
+      output: process.stdout
+    });
+
+    rl.on('line', line => line);
+
+    rl.on('close', async () => {
+      process.stdout.write(messages.getCurrentDirectoryMessage(currentPath));
+    });
 
   } catch (error) {
-      process.stderr.write(error.message);
+    process.stderr.write(error.message);
   }
 };
 
