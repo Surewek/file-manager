0eaca6a05e3e8fe973d9ffd2aa4de7fec5b4245f
Vitaly Vegera <surewek@gmail.com>
2024-10-14 15:50:05 +0300

    feat: add mv command logic

--- src/files-commands/mv.js
+++ src/files-commands/mv.js
@@ -1,8 +1,27 @@
+import path from 'path';
+import fs from 'fs/promises'
+import cp from './cp.js';
+import errors from '../data/error-messages.js';
+
 async function mv(currentPath, commandArguments) {
   try {
+    const argumentsCount = commandArguments.length;
 
+    if (argumentsCount !== 2) {
+      throw new Error(errors.invalidInputMessage);
+    }
+
+    const [source, dest] = commandArguments;
+
+    const sourcePath = path.resolve(currentPath, source);
+
+    const isCopiedSuccsessfully = await cp(currentPath, commandArguments);
+
+    if (isCopiedSuccsessfully) {
+      await fs.unlink(sourcePath);
+    }
   } catch (error) {
-      process.stderr.write(error.message);
+    process.stderr.write(error.message);
   }
 };
 
